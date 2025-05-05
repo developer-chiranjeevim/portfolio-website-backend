536f18a347f9807012000962887a2aa3c1675e4d
chiranjeevi <developer.chiranjeevim@gmail.com>
2025-05-05 15:57:11 +0530

    Created Lambda Handler

--- lambda_function.py
+++ lambda_function.py
@@ -1,13 +1,20 @@
 import json
-
+from main import add_record
 
 def lambda_handler(event, context):
-    
+    data = json.loads(event['body'])
 
-    return {
-        "statusCode": 200,
-        "body": json.dumps('hello from lambda function')
-    }
+    response = add_record(data)
+    if(response):
+        return {
+            "statusCode": 200,
+            "body": json.dumps('record added successfully')
+        }
+    else:
+        return {
+            "statusCode" : 404,
+            "body": json.dumps('cannot add record')
+        }
 
 
 
